8966b1658e472c982959cb2870cf7148c64847ec
robcodonnell <robodonnell@Robs-MacBook-Pro.local>
2016-03-18 17:18:20 -0400

    linethrough

--- js/main.js
+++ js/main.js
@@ -3,7 +3,12 @@ var $list = $('.list');
 
 $('form').on('submit', function (e) {
   var $li = $('<li>');
+
   e.preventDefault();
   $li.html($input.val());
   $list.append($li);
+});
+
+$('ul').on('click', 'li', function (e) {
+  $(this).toggleClass('is-clicked');
 });
