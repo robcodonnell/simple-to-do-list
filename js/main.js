ca680e0994ea7823e4a0327de1265d59ce077679
robcodonnell <robodonnell@Robs-MacBook-Pro.local>
2016-03-18 17:14:52 -0400

    got er done

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var $input = $('#list-input');
+var $list = $('.list');
+
+$('form').on('submit', function (e) {
+  var $li = $('<li>');
+  e.preventDefault();
+  $li.html($input.val());
+  $list.append($li);
+});
